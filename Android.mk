e09972330cd60f91333ff0c3cb1ca41e979bd316
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-11-24 15:40:05 -0500

    Still Fixing Thing
    Still Fixing Thing

--- Android.mk
+++ Android.mk
@@ -19,32 +19,49 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files) \
 
-LOCAL_STATIC_JAVA_LIBRARIES += \ 
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v7-gridlayout \
-    android-support-v13 \
-    android-opt-bitmap \
-    owasp-html-sanitizer \
-    activation \
-    additionnal \
-    commons \
-    javamaildir \
-    mail
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-gridlayout
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13
+LOCAL_STATIC_JAVA_LIBRARIES += android-opt-bitmap
+LOCAL_STATIC_JAVA_LIBRARIES += owasp-html-sanitizer
+LOCAL_STATIC_JAVA_LIBRARIES += activation
+LOCAL_STATIC_JAVA_LIBRARIES += additionnal
+LOCAL_STATIC_JAVA_LIBRARIES += commons
+LOCAL_STATIC_JAVA_LIBRARIES += activation
+LOCAL_STATIC_JAVA_LIBRARIES += javamaildir
+LOCAL_STATIC_JAVA_LIBRARIES += mail
+
+#LOCAL_STATIC_JAVA_LIBRARIES += \ 
+    #android-support-v4 \
+    #android-support-v7-appcompat \
+    #android-support-v7-gridlayout \
+    #android-support-v13 \
+    #android-opt-bitmap \
+    #owasp-html-sanitizer \
+    #activation \
+    #additionnal \
+    #commons \
+    #javamaildir \
+    #mail
 
 LOCAL_SDK_VERSION := current
 
 LOCAL_PACKAGE_NAME := Notes
 
 include $(BUILD_PACKAGE)
-
+##################################################
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += \
-    activation:libs/activation.jar \
-    additionnal:libs/additionnal.jar \
-    commons:libs/commons-io-2.4.jar \
-    javamaildir:libs/javamaildir-0.6.jar \
-    mail:libs/mail.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += activation:libs/activation.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += activation:libs/additionnal.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += activation:libs/commons-io-2.4.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += activation:libs/javamaildir-0.6.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += activation:libs/mail.jar
+#LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \   
+    #additionnal:libs/additionnal.jar \
+    #commons:libs/commons-io-2.4.jar \
+    #javamaildir:libs/javamaildir-0.6.jar \
+    #mail:libs/mail.jar
 
 include $(BUILD_MULTI_PREBUILT)
